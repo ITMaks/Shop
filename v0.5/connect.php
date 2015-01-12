d8ad7ed3af71c345227a31a7297774eb75723c97
ITMaks <itmaks77@gmail.com>
2015-01-12 15:27:57 +0200

    v0.5

---
+++ v0.5/connect.php
@@ -0,0 +1,6 @@
+<?php
+	mysql_connect('localhost','root','u8Ux8FjytUBVjMVL');
+	mysql_select_db('databaze');
+	mysql_query("SET NAMES UTF8");
+	mysql_query("SET CHARACTER SET UTF8");
+?>
---
+++ v0.5/settings.php
@@ -0,0 +1,24 @@
+<?php
+    $sitename = "Магазин аккаунтов";
+    $sitedesc = "Купить аккаунты";
+
+    $sitedomain = "http://maksdm.pp.ua/";
+    $sitedomaincopy = "maksdm.pp.ua";
+    $sitecopyyears = "2012-2014";
+    $sitecopy = "All rights reserved";
+
+    $systemlang = "ru"; //ru - Russian;
+
+    $interid = "54987dc6bf4efc541e2fa638";
+
+    ## CONTACTS ##
+    $skype = "ladykrys003";
+    $vk = "itmakssss";
+    $vkname= "Максим Дмитриев";
+    $icq = "1111-2222";
+    $siteheart = 753408; //ID siteheart
+
+    ## EMAIL ##
+    $suc_title = "Вы успешно приобрели товар на сайте " . $sitedomaincopy;
+    $suc_text = "Информация о купленном товаре: ";
+?>
